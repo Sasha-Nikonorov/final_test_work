e2813f783ebb8af58cc8c22fe4594e3bd1749f05
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-09-18 20:31:49 +0300

    add function print array

---
+++ test_array/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i != array.Length - 1)
+            Console.Write(", ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
